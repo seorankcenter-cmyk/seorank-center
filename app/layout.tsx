9646b3da9b1197fc83b9e56f0818e1cd4e89011a
seorankcenter-cmyk <seorankcenter@gmail.com>
2026-07-03 22:30:48 +0600

    Fix logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,10 @@ export const metadata: Metadata = {
   description: "Professional Local SEO & Web Development Services",
 
   icons: {
-  icon: "/seorank-logo.jpg",
-},
+    icon: "/seorank-logo.jpg",
+    shortcut: "/seorank-logo.jpg",
+    apple: "/seorank-logo.jpg",
+  },
 
   keywords: [
     "Local SEO",
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar() {
         {/* Logo */}
         <Link href="/" className="flex items-center">
           <Image
-            src="/seorank-logo.png"
+            src="/seorank-logo.jpg"
             alt="SEOrank Center"
             width={700}
             height={220}
